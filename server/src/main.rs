dbc34ca3fb12f3c235f5c4cb00f8394e7413f45c
Crinax <agreedeth@gmail.com>
2026-01-30 18:03:35 +0300

    feat(app): add sea-orm and log crates, set up sea-orm migration and features

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,17 @@
 use axum::Router;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() {
+    // TODO: implement db connection
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let app = Router::new().route("/", axum::routing::get(|| async { "Hello, World!" }));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3003")
         .await
         .unwrap();
+    log::info!(target: "application", "Listening on http://127.0.0.1:3003");
 
     axum::serve(listener, app).await.unwrap();
 }
