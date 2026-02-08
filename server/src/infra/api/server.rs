74d0a9dde2f509dac60614c82b256de774889704
Crinax <agreedeth@gmail.com>
2026-02-09 02:14:18 +0300

    refactor(app): move master creation to db module

--- server/src/infra/api/server.rs
+++ server/src/infra/api/server.rs
@@ -27,34 +27,8 @@ pub fn init_router(config: Arc<Config>, connection: DatabaseConnection) -> Route
         .nest("/api/v1/auth", create_auth_router(connection))
 }
 
-pub async fn create_master_user(connection: DatabaseConnection) {
-    let user_repository = UserRepository::new(connection);
-
-    if let Err(DomainError::ExternalServiceError(LoadUserByNamePortError::NotFound)) =
-        user_repository
-            .load_user_by_name(&UserName::new("admin".to_owned()))
-            .await
-    {
-        let registration_service =
-            RegistrationService::new(user_repository, ArgonPasswordSystem, UuidGenerator);
-
-        registration_service
-            .register(RegistrationCommand::new(
-                UserName::new("admin".to_owned()),
-                Secret::new("password".to_owned()),
-            ))
-            .await
-            .expect("Failed to create master user");
-
-        log::info!(target: "application", "Created user with name: `admin` and password: `password`");
-    }
-}
-
 pub async fn start_server(config: Arc<Config>, connection: DatabaseConnection) {
     let app = init_router(config.clone(), connection.clone());
-
-    log::info!(target: "application", "Creating master user");
-    create_master_user(connection).await;
 
     let listener = TcpListener::bind(config.socket_addr()).await.unwrap();
 
--- server/src/infra/db.rs
+++ server/src/infra/db.rs
@@ -1,10 +1,14 @@
 pub mod entities;
 mod repositories;
 
+use domain::prelude::*;
 use migration::{Migrator, MigratorTrait};
 use sea_orm::{Database, DatabaseConnection};
 
-use crate::infra::config::Config;
+use crate::{
+    application::RegistrationService,
+    infra::{config::Config, id_generator::UuidGenerator, security::ArgonPasswordSystem},
+};
 
 pub use repositories::*;
 
@@ -20,5 +24,31 @@ pub async fn init_db(config: &Config) -> DatabaseConnection {
         .await
         .expect("Failed to run database migrations");
 
+    log::info!(target: "application::db", "Creating master user");
+    create_master_user(connection.clone()).await;
+
     connection
+}
+
+pub async fn create_master_user(connection: DatabaseConnection) {
+    let user_repository = UserRepository::new(connection);
+
+    if let Err(DomainError::ExternalServiceError(LoadUserByNamePortError::NotFound)) =
+        user_repository
+            .load_user_by_name(&UserName::new("admin".to_owned()))
+            .await
+    {
+        let registration_service =
+            RegistrationService::new(user_repository, ArgonPasswordSystem, UuidGenerator);
+
+        registration_service
+            .register(RegistrationCommand::new(
+                UserName::new("admin".to_owned()),
+                Secret::new("password".to_owned()),
+            ))
+            .await
+            .expect("Failed to create master user");
+
+        log::info!(target: "application", "Created user with name: `admin` and password: `password`");
+    }
 }
